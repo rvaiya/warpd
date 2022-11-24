rvaiya/warpd

commit 15631a58beda2c775af12b5b5e030cb4fc4289df

wayland: Optimize wayland code
 - Minimize wl_shm_pool profligacy by doing pixel math on a shared memory pool
   instead of creating one for each surface (i.e use a single backing buffer).
 - Switch to 'way_' instead of 'wl_' as an internal prefix to keep
   names distinct from wayland library objects

This significantly reduces memory usage and should improve performance, though
there is still room for improvement (batching/caching draw ops).

src/platform/linux/wayland/input.c
@@ -8,16 +8,15 @@
 static struct input_event input_queue[32];
 static size_t input_queue_sz;
 
-static struct surface input_pixel;
 static uint8_t x_active_mods = 0;
 static int infd = 0;
 
 static void noop() {}
 struct keymap_entry keymap[256] = {0};
 
-void update_mods(uint8_t code, uint8_t pressed)
+static void update_mods(uint8_t code, uint8_t pressed)
 {
-	const char *name = wl_input_lookup_name(code, 0);
+	const char *name = way_input_lookup_name(code, 0);
 
 	if (!name)
 		return;
@@ -109,34 +108,6 @@ static void handle_keymap(void *data,
 	xkb_context_unref(ctx);
 }
 
-void handle_pointer_enter(void *data,
-			  struct wl_pointer *wl_pointer,
-			  uint32_t serial,
-			  struct wl_surface *surface, wl_fixed_t wlx,
-			  wl_fixed_t wly)
-{
-
-	if (active_screen->ptrx == -1 && surface == active_screen->overlay->wl_surface) {
-		int x = wl_fixed_to_int(wlx);
-		int y = wl_fixed_to_int(wly);
-
-		active_screen->ptrx = x;
-		active_screen->ptry = y;
-	}
-}
-
-struct wl_pointer_listener wl_pointer_listener = {
-	.enter = handle_pointer_enter,
-	.leave = noop,
-	.motion = noop,
-	.button = noop,
-	.axis = noop,
-	.frame = noop,
-	.axis_source = noop,
-	.axis_stop = noop,
-	.axis_discrete = noop,
-};
-
 static struct wl_keyboard_listener wl_keyboard_listener = {
 	.key = handle_key,
 	.keymap = handle_keymap,
@@ -146,24 +117,24 @@ static struct wl_keyboard_listener wl_keyboard_listener = {
 	.repeat_info = noop,
 };
 
-void wl_input_ungrab_keyboard()
-{
-	surface_hide(&input_pixel);
-}
-
+static struct surface *input_surface = NULL;
 /*
  * *Note*: Wayland does not allow for global input handlers.
  * Input events can only be captured by a focused window :(.
  */
-void wl_input_grab_keyboard()
+void way_input_grab_keyboard()
 {
-	if (!input_pixel.buf)
-		init_surface(&input_pixel, -1, -1, 1, 1, 1);
-
-	surface_show(&input_pixel, NULL);
+	input_surface = create_surface(&screens[0], -1, -1, 1, 1, 1);
 }
 
-struct input_event *wl_input_next_event(int timeout)
+void way_input_ungrab_keyboard()
+{
+	destroy_surface(input_surface);
+	input_surface = NULL;
+}
+
+
+struct input_event *way_input_next_event(int timeout)
 {
 	static struct input_event ev;
 
@@ -189,9 +160,7 @@ struct input_event *wl_input_next_event(int timeout)
 	}
 }
 
-void add_seat(struct wl_seat *seat)
+void init_input()
 {
-	wl_keyboard_add_listener(wl_seat_get_keyboard(seat), &wl_keyboard_listener, NULL);
-	wl_pointer_add_listener(wl_seat_get_pointer(seat), &wl_pointer_listener, NULL);
-	wl_seat_destroy(seat);
+	wl_keyboard_add_listener(wl_seat_get_keyboard(wl.seat), &wl_keyboard_listener, NULL);
 }

src/platform/linux/wayland/screen.c
@@ -5,14 +5,12 @@
  */
 #include "wayland.h"
 
-struct screen *active_screen = NULL;
-
 static void noop() {}
 
-void xdg_output_handle_logical_position(void *data,
-					struct zxdg_output_v1 *zxdg_output_v1,
-					int32_t x,
-					int32_t y)
+static void xdg_output_handle_logical_position(void *data,
+					       struct zxdg_output_v1
+					       *zxdg_output_v1, int32_t x,
+					       int32_t y)
 {
 	struct screen *scr = data;
 
@@ -21,10 +19,10 @@ void xdg_output_handle_logical_position(void *data,
 	scr->state++;
 }
 
-void xdg_output_handle_logical_size(void *data,
-				    struct zxdg_output_v1 *zxdg_output_v1,
-				    int32_t w,
-				    int32_t h)
+static void xdg_output_handle_logical_size(void *data,
+					   struct zxdg_output_v1
+					   *zxdg_output_v1, int32_t w,
+					   int32_t h)
 {
 	struct screen *scr = data;
 
@@ -41,52 +39,140 @@ static struct zxdg_output_v1_listener zxdg_output_v1_listener = {
 	.description = noop,
 };
 
+static void handle_pointer_enter(void *data,
+				 struct wl_pointer *wl_pointer,
+				 uint32_t serial,
+				 struct wl_surface *surface,
+				 wl_fixed_t wlx, wl_fixed_t wly)
+{
+	int i;
+	struct surface **overlays = data;
+
+	if (!ptr.scr) {
+		ptr.x = wl_fixed_to_int(wlx);
+		ptr.y = wl_fixed_to_int(wly);
+
+		for (i = 0; i < nr_screens; i++) {
+			if (surface == surface_get_wl_surface(overlays[i])) {
+				ptr.scr = &screens[i];
+			}
+		}
+	}
+}
+
+static struct wl_pointer_listener wl_pointer_listener = {
+	.enter = handle_pointer_enter,
+	.leave = noop,
+	.motion = noop,
+	.button = noop,
+	.axis = noop,
+	.frame = noop,
+	.axis_source = noop,
+	.axis_stop = noop,
+	.axis_discrete = noop,
+};
+
+/* 
+ * Register a pointer_listener and listen for enter events after
+ * creating a full screen surface for each screen in order to capture the initial
+ * cursor position. I couldn't find a better way to achieve this :/.
+ */
+static void discover_pointer_location()
+{
+	size_t i;
+	static struct surface *overlays[MAX_SCREENS];
+
+	wl_pointer_add_listener(wl_seat_get_pointer(wl.seat), &wl_pointer_listener, overlays);
+
+	for (i = 0; i < nr_screens; i++) {
+		struct screen *scr = &screens[i];
+		overlays[i] = create_surface(&screens[i], 0, 0, screens[i].w, screens[i].h, 0);
+	}
+
+	while (!ptr.scr)
+		wl_display_dispatch(wl.dpy);
+
+	for (i = 0; i < nr_screens; i++)
+		destroy_surface(overlays[i]);
+}
+
 void add_screen(struct wl_output *output)
 {
 	struct screen *scr = &screens[nr_screens++];
 	scr->wl_output = output;
 }
 
-void wl_screen_draw_box(struct screen *scr, int x, int y, int w, int h, const char *color)
+void way_screen_draw_box(struct screen *scr, int x, int y, int w, int h, const char *color)
 {
 	uint8_t r, g, b, a;
 
-	assert(scr->nr_surfaces < MAX_SURFACES);
-	struct surface *sfc = &scr->surfaces[scr->nr_surfaces++];
+	assert(scr->nr_boxes < MAX_BOXES);
 
-	init_surface(sfc, x, y, w, h, 0);
+	way_hex_to_rgba(color, &r, &g, &b, &a);
+	cairo_set_source_rgba(scr->cr, r / 255.0, g / 255.0, b / 255.0, a / 255.0);
+	cairo_rectangle(scr->cr, x, y, w, h);
+	cairo_fill(scr->cr);
 
-	wl_hex_to_rgba(color, &r, &g, &b, &a);
-	cairo_set_source_rgba(sfc->cr, r / 255.0, g / 255.0, b / 255.0, a / 255.0);
-	cairo_paint(sfc->cr);
-
-	surface_show(sfc, scr->wl_output);
+	scr->boxes[scr->nr_boxes++] = create_surface(scr, x, y, w, h, 0);
 }
 
 
-void wl_screen_get_dimensions(struct screen *scr, int *w, int *h)
+void way_screen_get_dimensions(struct screen *scr, int *w, int *h)
 {
 	*w = scr->w;
 	*h = scr->h;
 }
 
-void wl_screen_clear(struct screen *scr)
+void way_screen_clear(struct screen *scr)
 {
 	size_t i;
-	for (i = 0; i < scr->nr_surfaces; i++)
-		surface_destroy(&scr->surfaces[i]);
+	for (i = 0; i < scr->nr_boxes; i++)
+		destroy_surface(scr->boxes[i]);
 
-	scr->nr_surfaces = 0;
-	bzero(scr->overlay->buf, scr->overlay->bufsz);
-	surface_hide(scr->overlay);
+	destroy_surface(scr->hints);
+
+	scr->nr_boxes = 0;
+	scr->hints = NULL;
+}
+
+static void init_screen_pool(struct screen *scr)
+{
+	int fd;
+	static int shm_num = 0;
+	char shm_path[64];
+	size_t bufsz;
+	char *buf;
+	cairo_surface_t *cairo_surface;
+
+	scr->stride = cairo_format_stride_for_width(CAIRO_FORMAT_ARGB32, scr->w);
+
+	bufsz = scr->stride * scr->h + scr->w * 4;
+	sprintf(shm_path, "/warpd_%d", shm_num++);
+
+	fd = shm_open(shm_path, O_CREAT|O_TRUNC|O_RDWR, 0600);
+	if (fd < 0) {
+		perror("shm_open");
+		exit(-1);
+	}
+
+	ftruncate(fd, bufsz);
+
+	scr->wl_pool = wl_shm_create_pool(wl.shm, fd, bufsz);
+	buf = mmap(NULL, bufsz, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+	close(fd);
+
+	cairo_surface = cairo_image_surface_create_for_data(buf,
+							    CAIRO_FORMAT_ARGB32, scr->w,
+							    scr->h, scr->stride);
+	scr->cr = cairo_create(cairo_surface);
 }
 
 void init_screen()
 {
 	size_t i;
-	struct surface sfc;
 
 	for (i = 0; i < nr_screens; i++) {
+		struct surface *sfc;
 		struct screen *scr = &screens[i];
 
 		scr->xdg_output =
@@ -103,21 +189,9 @@ void init_screen()
 
 		scr->ptrx = -1;
 		scr->ptry = -1;
-		scr->overlay = calloc(1, sizeof(struct surface));
-		init_surface(scr->overlay, 0, 0, scr->w, scr->h, 0);
+
+		init_screen_pool(scr);
 	}
 
-	sfc.screen = 0;
-	init_surface(&sfc, 0, 0, 1, 1, 0);
-	surface_show(&sfc, NULL);
-	while(!sfc.screen)
-		wl_display_dispatch(wl.dpy);
-
-	active_screen = sfc.screen;
-	surface_destroy(&sfc);
-
-	surface_show(active_screen->overlay, active_screen->wl_output);
-	while (active_screen->ptrx == -1)
-		wl_display_dispatch(wl.dpy);
-	surface_hide(active_screen->overlay);
+	discover_pointer_location();
 }

src/platform/linux/wayland/surface.c
@@ -9,10 +9,25 @@ static void noop() {}
 
 /* TODO: add support for fractional scaling (requires xdg-output?). */
 
-void layer_surface_handle_configure(void *data,
-				    struct zwlr_layer_surface_v1
-				    *layer_surface, uint32_t serial,
-				    uint32_t width, uint32_t height)
+/* A 'surface' in the local context can be thought of as a viewport into a
+ * rectangular region of the screen's backing buffer (the wl_shm_pool).  It is
+ * undergirded by a corresponding wayland surface and wayland layer surface with a
+ * wayland buffer object created from the relevant part of the screen's memory
+ * pool. Surfaces are visible as long as they exist, and hiding them is achieved
+ * by destroying them.
+ */
+struct surface {
+	struct zwlr_layer_surface_v1 *wl_layer_surface;
+	struct wl_surface *wl_surface;
+	struct wl_buffer *wl_buffer;
+
+	int configured;
+	int destroyed;
+};
+
+static void layer_surface_handle_configure(void *data, struct zwlr_layer_surface_v1
+					   *layer_surface, uint32_t serial,
+					   uint32_t width, uint32_t height)
 {
 	struct surface *sfc = data;
 
@@ -35,124 +50,74 @@ static const struct zwlr_layer_surface_v1_listener layer_surface_listener = {
         .closed = noop,
 };
 
-static void surface_handle_enter(void *data,
-				 struct wl_surface *wl_surface,
-				 struct wl_output *output)
+void destroy_surface(struct surface *sfc)
 {
-	struct surface *sfc = data;
-	size_t i;
-
-	for (i = 0; i < nr_screens; i++)
-		if (screens[i].wl_output == output)
-			sfc->screen = &screens[i];
-}
-
-static struct wl_surface_listener wl_surface_listener = {
-	.enter = surface_handle_enter,
-	.leave = noop,
-};
-
-/* TODO: proper cleanup */
-void init_surface(struct surface *sfc, int x, int y, int w, int h, int input_focus)
-{
-	int fd;
-	struct wl_shm_pool *pool;
-	cairo_surface_t *cairo_surface;
-	char shm_path[64];
-	static int shm_num = 0;
-
-	sfc->x = x;
-	sfc->y = y;
-	sfc->w = w;
-	sfc->h = h;
-
-	sfc->input_focus = input_focus;
-
-	sfc->stride = cairo_format_stride_for_width(CAIRO_FORMAT_ARGB32, w);
-	sfc->bufsz = h*sfc->stride;
-
-	sprintf(shm_path, "/warpd_%d", shm_num++);
-
-	fd = shm_open(shm_path, O_CREAT|O_TRUNC|O_RDWR, 0600); //TODO: fixme
-	if (fd < 0) {
-		perror("shm_open");
-		exit(-1);
-	}
-
-	ftruncate(fd, sfc->bufsz);
-
-	pool = wl_shm_create_pool(wl.shm, fd, sfc->bufsz);
-
-	sfc->buf = mmap(NULL, sfc->bufsz, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0); //OPT
-	sfc->wl_buffer = wl_shm_pool_create_buffer(pool, 0, sfc->w, sfc->h, sfc->stride, WL_SHM_FORMAT_ARGB8888);
-
-	wl_shm_pool_destroy(pool);
-
-	close(fd);
-
-	sfc->wl_surface = NULL;
-	sfc->wl_layer_surface = NULL;
-
-	cairo_surface = cairo_image_surface_create_for_data(sfc->buf,
-							    CAIRO_FORMAT_ARGB32, sfc->w,
-							    sfc->h, sfc->stride);
-	sfc->cr = cairo_create(cairo_surface);
-}
-
-
-void surface_destroy(struct surface *sfc)
-{
-	munmap(sfc->buf, sfc->bufsz);
-	wl_surface_destroy(sfc->wl_surface);
-	zwlr_layer_surface_v1_destroy(sfc->wl_layer_surface);
-
-	wl_buffer_destroy(sfc->wl_buffer);
-}
-
-void surface_hide(struct surface *sfc)
-{
-	if (sfc->wl_layer_surface) {
+	if (sfc) {
 		zwlr_layer_surface_v1_destroy(sfc->wl_layer_surface);
 		wl_surface_destroy(sfc->wl_surface);
+		wl_buffer_destroy(sfc->wl_buffer);
 
-		sfc->wl_surface = NULL;
-		sfc->wl_layer_surface = NULL;
+		free(sfc);
 	}
 }
 
-void surface_show(struct surface *sfc, struct wl_output *output)
-{
-	if (sfc->wl_surface)
-		return;
+/* All surfaces which exist are visible. Surface creation/destruction is the means
+ * of displaying content on the screen (there is no show/hide) and should be considered a cheap operation
+ * which operates on a persistent shared buffer (memory pool). */
 
+struct surface *create_surface(struct screen *scr, int x, int y, int w, int h, int capture_input)
+{
+	struct surface *sfc = calloc(1, sizeof (struct surface));
+
+	if (x < 0) {
+		x = 0;
+		w += x;
+	}
+	if (y < 0) {
+		y = 0;
+		h += y;
+	}
+	if ((x+w) > scr->w)
+		x = scr->w-w;
+	if ((y+h) > scr->h)
+		y = scr->h-h;
+
+	sfc->wl_buffer = wl_shm_pool_create_buffer(scr->wl_pool, y*scr->stride + x*4, w, h, scr->stride, WL_SHM_FORMAT_ARGB8888);
+	assert(sfc->wl_buffer);
 	sfc->wl_surface = wl_compositor_create_surface(wl.compositor);
+
+	assert(sfc->wl_surface);
 	sfc->wl_layer_surface =
 		zwlr_layer_shell_v1_get_layer_surface(wl.layer_shell, sfc->wl_surface,
-						      output,
+						      scr->wl_output,
 						      ZWLR_LAYER_SHELL_V1_LAYER_OVERLAY,
 						      "warpd");
 
-	wl_surface_add_listener(sfc->wl_surface, &wl_surface_listener, sfc);
-
 	assert(sfc->wl_layer_surface);
-	zwlr_layer_surface_v1_add_listener(sfc->wl_layer_surface,
-					   &layer_surface_listener,
-					   sfc);
 
 	zwlr_layer_surface_v1_set_size(sfc->wl_layer_surface, 10, 10);
 	zwlr_layer_surface_v1_set_anchor(sfc->wl_layer_surface, ZWLR_LAYER_SURFACE_V1_ANCHOR_TOP|ZWLR_LAYER_SURFACE_V1_ANCHOR_LEFT);
-	zwlr_layer_surface_v1_set_margin(sfc->wl_layer_surface, sfc->y, 0, 0, sfc->x);
+	zwlr_layer_surface_v1_set_margin(sfc->wl_layer_surface, y, 0, 0, x);
 	zwlr_layer_surface_v1_set_exclusive_zone(sfc->wl_layer_surface, -1);
 
-	if (sfc->input_focus)
+	zwlr_layer_surface_v1_add_listener(sfc->wl_layer_surface, &layer_surface_listener, sfc);
+
+	if (capture_input)
 		zwlr_layer_surface_v1_set_keyboard_interactivity(sfc->wl_layer_surface,
 								 ZWLR_LAYER_SURFACE_V1_KEYBOARD_INTERACTIVITY_EXCLUSIVE);
 
-	wl_surface_commit(sfc->wl_surface);
 
+	wl_surface_commit(sfc->wl_surface);
 	sfc->configured = 0;
 	while (!sfc->configured)
 		wl_display_dispatch(wl.dpy);
 
 	wl_display_flush(wl.dpy);
+
+	return sfc;
+}
+
+struct wl_surface *surface_get_wl_surface(struct surface *sfc)
+{
+	return sfc->wl_surface;
 }

src/platform/linux/wayland/wayland.c
@@ -25,9 +25,11 @@ static struct {
 	{"backspace", "BackSpace"},
 };
 
+struct ptr ptr = {0};
+
 /* Input */
 
-uint8_t wl_input_lookup_code(const char *name, int *shifted)
+uint8_t way_input_lookup_code(const char *name, int *shifted)
 {
 	size_t i;
 
@@ -47,7 +49,7 @@ uint8_t wl_input_lookup_code(const char *name, int *shifted)
 	return 0;
 }
 
-const char *wl_input_lookup_name(uint8_t code, int shifted)
+const char *way_input_lookup_name(uint8_t code, int shifted)
 {
 	size_t i;
 	const char *name = NULL;
@@ -64,7 +66,7 @@ const char *wl_input_lookup_name(uint8_t code, int shifted)
 	return name;
 }
 
-void wl_mouse_move(struct screen *scr, int x, int y)
+void way_mouse_move(struct screen *scr, int x, int y)
 {
 	size_t i;
 	int maxx = INT_MIN;
@@ -72,8 +74,9 @@ void wl_mouse_move(struct screen *scr, int x, int y)
 	int minx = INT_MAX;
 	int miny = INT_MAX;
 
-	active_screen->ptrx = x;
-	active_screen->ptry = y;
+	ptr.x = x;
+	ptr.y = y;
+	ptr.scr = scr;
 
 	for (i = 0; i < nr_screens; i++) {
 		int x = screens[i].x + screens[i].w;
@@ -101,7 +104,6 @@ void wl_mouse_move(struct screen *scr, int x, int y)
 						wl_fixed_from_int(maxy-miny));
 	zwlr_virtual_pointer_v1_frame(wl.ptr);
 
-	active_screen = scr;
 	wl_display_flush(wl.dpy);
 }
 
@@ -112,19 +114,19 @@ void wl_mouse_move(struct screen *scr, int x, int y)
 		case 3: btn = 273;break; \
 	}
 
-void wl_mouse_down(int btn)
+void way_mouse_down(int btn)
 {
 	normalize_btn(btn);
 	zwlr_virtual_pointer_v1_button(wl.ptr, 0, btn, 1);
 }
 
-void wl_mouse_up(int btn)
+void way_mouse_up(int btn)
 {
 	normalize_btn(btn);
 	zwlr_virtual_pointer_v1_button(wl.ptr, 0, btn, 0);
 }
 
-void wl_mouse_click(int btn)
+void way_mouse_click(int btn)
 {
 	normalize_btn(btn);
 
@@ -135,26 +137,26 @@ void wl_mouse_click(int btn)
 	wl_display_flush(wl.dpy);
 }
 
-void wl_mouse_get_position(struct screen **scr, int *x, int *y)
+void way_mouse_get_position(struct screen **scr, int *x, int *y)
 {
 	if (scr)
-		*scr = active_screen;
+		*scr = ptr.scr;
 	if (x)
-		*x = active_screen->ptrx;
+		*x = ptr.x;
 	if (y)
-		*y = active_screen->ptry;
+		*y = ptr.y;
 }
 
-void wl_mouse_show()
+void way_mouse_show()
 {
 }
 
-void wl_mouse_hide()
+void way_mouse_hide()
 {
 	fprintf(stderr, "wayland: mouse hiding not implemented\n");
 }
 
-void wl_scroll(int direction)
+void way_scroll(int direction)
 {
 	//TODO: add horizontal scroll
 	direction = direction == SCROLL_DOWN ? 1 : -1;
@@ -168,10 +170,10 @@ void wl_scroll(int direction)
 	wl_display_flush(wl.dpy);
 }
 
-void wl_copy_selection() { UNIMPLEMENTED }
-struct input_event *wl_input_wait(struct input_event *events, size_t sz) { UNIMPLEMENTED }
+void way_copy_selection() { UNIMPLEMENTED }
+struct input_event *way_input_wait(struct input_event *events, size_t sz) { UNIMPLEMENTED }
 
-void wl_screen_list(struct screen *scr[MAX_SCREENS], size_t *n)
+void way_screen_list(struct screen *scr[MAX_SCREENS], size_t *n)
 {
 	size_t i;
 	for (i = 0; i < nr_screens; i++)
@@ -180,38 +182,51 @@ void wl_screen_list(struct screen *scr[MAX_SCREENS], size_t *n)
 	*n = nr_screens;
 }
 
-void wl_monitor_file(const char *path) { UNIMPLEMENTED }
+void way_monitor_file(const char *path) { UNIMPLEMENTED }
 
-void wl_commit()
+void way_commit()
 {
+	wl_display_flush(wl.dpy);
+	wl_display_dispatch_pending(wl.dpy);
+}
+
+static void cleanup()
+{
+	/* wlroots doesn't seem to like it if we terminate before button up :/ */
+	zwlr_virtual_pointer_v1_button(wl.ptr, 0, 272, 0);
+	zwlr_virtual_pointer_v1_button(wl.ptr, 0, 274, 0);
+	zwlr_virtual_pointer_v1_button(wl.ptr, 0, 273, 0);
+	wl_display_flush(wl.dpy);
 }
 
 void wayland_init(struct platform *platform)
 {
-	wl_init();
+	way_init();
 
-	platform->monitor_file = wl_monitor_file;
+	platform->monitor_file = way_monitor_file;
 
-	platform->commit = wl_commit;
-	platform->copy_selection = wl_copy_selection;
-	platform->hint_draw = wl_hint_draw;
-	platform->init_hint = wl_init_hint;
-	platform->input_grab_keyboard = wl_input_grab_keyboard;
-	platform->input_lookup_code = wl_input_lookup_code;
-	platform->input_lookup_name = wl_input_lookup_name;
-	platform->input_next_event = wl_input_next_event;
-	platform->input_ungrab_keyboard = wl_input_ungrab_keyboard;
-	platform->input_wait = wl_input_wait;
-	platform->mouse_click = wl_mouse_click;
-	platform->mouse_down = wl_mouse_down;
-	platform->mouse_get_position = wl_mouse_get_position;
-	platform->mouse_hide = wl_mouse_hide;
-	platform->mouse_move = wl_mouse_move;
-	platform->mouse_show = wl_mouse_show;
-	platform->mouse_up = wl_mouse_up;
-	platform->screen_clear = wl_screen_clear;
-	platform->screen_draw_box = wl_screen_draw_box;
-	platform->screen_get_dimensions = wl_screen_get_dimensions;
-	platform->screen_list = wl_screen_list;
-	platform->scroll = wl_scroll;
+	atexit(cleanup);
+
+	platform->commit = way_commit;
+	platform->copy_selection = way_copy_selection;
+	platform->hint_draw = way_hint_draw;
+	platform->init_hint = way_init_hint;
+	platform->input_grab_keyboard = way_input_grab_keyboard;
+	platform->input_lookup_code = way_input_lookup_code;
+	platform->input_lookup_name = way_input_lookup_name;
+	platform->input_next_event = way_input_next_event;
+	platform->input_ungrab_keyboard = way_input_ungrab_keyboard;
+	platform->input_wait = way_input_wait;
+	platform->mouse_click = way_mouse_click;
+	platform->mouse_down = way_mouse_down;
+	platform->mouse_get_position = way_mouse_get_position;
+	platform->mouse_hide = way_mouse_hide;
+	platform->mouse_move = way_mouse_move;
+	platform->mouse_show = way_mouse_show;
+	platform->mouse_up = way_mouse_up;
+	platform->screen_clear = way_screen_clear;
+	platform->screen_draw_box = way_screen_draw_box;
+	platform->screen_get_dimensions = way_screen_get_dimensions;
+	platform->screen_list = way_screen_list;
+	platform->scroll = way_scroll;
 }

commit 2f29c370989e1f49574927d4e5051654aec44200

wayland: Fix multi-screen regression introduced in 15631a5

src/platform/linux/wayland/input.c
@@ -108,11 +108,30 @@ static void handle_keymap(void *data,
 	xkb_context_unref(ctx);
 }
 
+static int input_grabbed = 0;
+static void handle_enter(void *data,
+			 struct wl_keyboard *wl_keyboard,
+			 uint32_t serial,
+			 struct wl_surface *surface,
+			 struct wl_array *keys)
+{
+	input_grabbed = 1;
+}
+
+static void handle_leave(void *data,
+			 struct wl_keyboard *wl_keyboard,
+			 uint32_t serial,
+			 struct wl_surface *surface)
+{
+	input_grabbed = 0;
+}
+
+
 static struct wl_keyboard_listener wl_keyboard_listener = {
 	.key = handle_key,
 	.keymap = handle_keymap,
-	.enter = noop,
-	.leave = noop,
+	.enter = handle_enter,
+	.leave = handle_leave,
 	.modifiers = noop,
 	.repeat_info = noop,
 };
@@ -125,11 +144,16 @@ static struct surface *input_surface = NULL;
 void way_input_grab_keyboard()
 {
 	input_surface = create_surface(&screens[0], -1, -1, 1, 1, 1);
+	surface_show(input_surface);
+	while (!input_grabbed)
+		wl_display_dispatch(wl.dpy);
 }
 
 void way_input_ungrab_keyboard()
 {
 	destroy_surface(input_surface);
+	while (!input_grabbed)
+		wl_display_dispatch(wl.dpy);
 	input_surface = NULL;
 }
 

src/platform/linux/wayland/screen.c
@@ -46,16 +46,15 @@ static void handle_pointer_enter(void *data,
 				 wl_fixed_t wlx, wl_fixed_t wly)
 {
 	int i;
-	struct surface **overlays = data;
 
 	if (!ptr.scr) {
 		ptr.x = wl_fixed_to_int(wlx);
 		ptr.y = wl_fixed_to_int(wly);
 
 		for (i = 0; i < nr_screens; i++) {
-			if (surface == surface_get_wl_surface(overlays[i])) {
-				ptr.scr = &screens[i];
-			}
+			struct screen *scr = &screens[i];
+			if (scr->overlay && surface == surface_get_wl_surface(scr->overlay))
+				ptr.scr = scr;
 		}
 	}
 }
@@ -80,25 +79,31 @@ static struct wl_pointer_listener wl_pointer_listener = {
 static void discover_pointer_location()
 {
 	size_t i;
-	static struct surface *overlays[MAX_SCREENS];
 
-	wl_pointer_add_listener(wl_seat_get_pointer(wl.seat), &wl_pointer_listener, overlays);
+	wl_pointer_add_listener(wl_seat_get_pointer(wl.seat), &wl_pointer_listener, NULL);
 
 	for (i = 0; i < nr_screens; i++) {
 		struct screen *scr = &screens[i];
-		overlays[i] = create_surface(&screens[i], 0, 0, screens[i].w, screens[i].h, 0);
+		scr->overlay = create_surface(scr, 0, 0, scr->w, scr->h, 0);
 	}
+
+	for (i = 0; i < nr_screens; i++)
+		surface_show(screens[i].overlay);
 
 	while (!ptr.scr)
 		wl_display_dispatch(wl.dpy);
 
-	for (i = 0; i < nr_screens; i++)
-		destroy_surface(overlays[i]);
+	for (i = 0; i < nr_screens; i++) {
+		struct screen *scr = &screens[i];
+		destroy_surface(scr->overlay);
+		scr->overlay = NULL;
+	}
 }
 
 void add_screen(struct wl_output *output)
 {
 	struct screen *scr = &screens[nr_screens++];
+	scr->overlay = NULL;
 	scr->wl_output = output;
 }
 

src/platform/linux/wayland/surface.c
@@ -22,6 +22,7 @@ struct surface {
 	struct wl_buffer *wl_buffer;
 
 	int configured;
+	int capture_input;
 	int destroyed;
 };
 
@@ -102,19 +103,25 @@ struct surface *create_surface(struct screen *scr, int x, int y, int w, int h, i
 
 	zwlr_layer_surface_v1_add_listener(sfc->wl_layer_surface, &layer_surface_listener, sfc);
 
-	if (capture_input)
-		zwlr_layer_surface_v1_set_keyboard_interactivity(sfc->wl_layer_surface,
-								 ZWLR_LAYER_SURFACE_V1_KEYBOARD_INTERACTIVITY_EXCLUSIVE);
-
-
-	wl_surface_commit(sfc->wl_surface);
+	sfc->capture_input = capture_input;
 	sfc->configured = 0;
-	while (!sfc->configured)
-		wl_display_dispatch(wl.dpy);
-
-	wl_display_flush(wl.dpy);
 
 	return sfc;
+}
+
+void surface_show(struct surface *sfc)
+{
+	wl_surface_commit(sfc->wl_surface);
+	wl_display_flush(wl.dpy);
+
+	if (sfc->capture_input) {
+		zwlr_layer_surface_v1_set_keyboard_interactivity(sfc->wl_layer_surface,
+								 ZWLR_LAYER_SURFACE_V1_KEYBOARD_INTERACTIVITY_EXCLUSIVE);
+	}
+
+
+	while (!sfc->configured)
+		wl_display_dispatch(wl.dpy);
 }
 
 struct wl_surface *surface_get_wl_surface(struct surface *sfc)

src/platform/linux/wayland/wayland.c
@@ -186,8 +186,17 @@ void way_monitor_file(const char *path) { UNIMPLEMENTED }
 
 void way_commit()
 {
-	wl_display_flush(wl.dpy);
-	wl_display_dispatch_pending(wl.dpy);
+	int i;
+	for (i = 0; i < nr_screens; i++) {
+		int j;
+		struct screen *scr = &screens[i];
+
+		for (j = 0; j < scr->nr_boxes; j++)
+			surface_show(scr->boxes[j]);
+
+		if (scr->hints)
+			surface_show(scr->hints);
+	}
 }
 
 static void cleanup()

src/platform/linux/wayland/wayland.h
@@ -55,6 +55,7 @@ struct screen {
 	size_t nr_boxes;
 	struct surface *boxes[MAX_BOXES];
 
+	struct surface *overlay;
 	struct surface *hints;
 
 	struct wl_output *wl_output;
@@ -97,6 +98,7 @@ extern struct wl wl;
 struct surface *create_surface(struct screen *scr, int x, int y, int w, int h, int capture_input);
 void destroy_surface(struct surface *sfc);
 struct wl_surface *surface_get_wl_surface(struct surface *sfc);
+void surface_show(struct surface *sfc);
 
 /* Exported platform functions. */
 void way_run(void (*init)(void));